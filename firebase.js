495eea69523f401f00074432a6ecb96a51152081
kdevante <144164265+kdevante@users.noreply.github.com>
2024-07-31 21:01:42 +0300

    Setup

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyB9ifPpnOjg2kWqkxAPvAs4xXZBX4Vode0",
+    authDomain: "inventory-management-afdb1.firebaseapp.com",
+    projectId: "inventory-management-afdb1",
+    storageBucket: "inventory-management-afdb1.appspot.com",
+    messagingSenderId: "1090664194478",
+    appId: "1:1090664194478:web:9a1014965a55fb6d7da83f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
